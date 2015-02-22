20f68e2d14eeb05fd730ec05f07f43d738fe6160
Kate <kbalducci@hotmail.com>
2015-02-22 16:54:53 -0600

    updated rooms show page to include meeting info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+    # Room.create(room_name: 'Ruby on Rails Room',
+    #             location: 'Building C',
+    #             description: 'Perfect room for small gatherings. Features a whiteboard and projector.
+    #             Design accents include an open ceiling concept and glass wall',
+    #             )
