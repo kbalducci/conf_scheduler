beb8aae5bd3cd3cf1f8127aaad14251bd9a6c538
Kate <kbalducci@hotmail.com>
2015-02-22 11:38:06 -0600

    updated rooms and room partial

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,6 +5,30 @@ class RoomsController < ApplicationController
     user = current_user
     @users = User.all
     @rooms = Room.all
+    @room = Room.new
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to @room, notice: 'User was successfully created.' }
+        format.json { render json: @room, status: :created, location: @room }
+      else
+        format.html { render "welcome/index" }
+        format.json { render json: @room.errors, status: :unprocessable_entity }
+      end
+    end
+    end
+
+  private
+  def room_params
+    params.require(:room).permit(:room_name, :location, :description, :photo)
   end
 
 end
