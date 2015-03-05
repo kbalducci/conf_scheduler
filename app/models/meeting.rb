37dcf62589d8dbf649c26b1db8aa0ca40e2f118c
Kate <kbalducci@hotmail.com>
2015-03-04 19:42:24 -0600

    updated twilio to be on user model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,23 +1,7 @@
-require 'twilio-ruby'
-
 class Meeting < ActiveRecord::Base
   belongs_to :user
   belongs_to :room
   has_many :comments
   validates_presence_of :start_time, :end_time, :title, reject_if: :all_blank
   validates :room_id, :title, presence: true
-
-  def self.establish_twilio
-    @account_sid = Figaro.env.twilio_sid
-    @auth_token = Figaro.env.auth_token
-    @from_phone_number = "512-456-3275"
-    @twilio_client = Twilio::REST::Client.new(@account_sid, @auth_token)
-  end
-
-  def self.send_text(body)
-    Appointment.establish_twilio
-    @twilio_client.messages.create(to: "512-731-1220",
-                                  from: @from_phone_number,
-                                  body: body)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -9,6 +11,20 @@ class User < ActiveRecord::Base
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+    def self.establish_twilio
+    @account_sid = Figaro.env.twilio_sid
+    @auth_token = Figaro.env.auth_token
+    @from_phone_number = "512-456-3275"
+    @twilio_client = Twilio::REST::Client.new(@account_sid, @auth_token)
+  end
+
+  def self.send_text(body)
+    Appointment.establish_twilio
+    @twilio_client.messages.create(to: "512-731-1220",
+                                  from: @from_phone_number,
+                                  body: body)
   end
 
 end
